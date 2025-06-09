a9c65ee08b144891c59431792d0b52f4ffd13420
Liberbell <liberbell@gmail.com>
2025-06-09 12:07:17 +0900

    get_rate3

--- lesson1/get_rate.py
+++ lesson1/get_rate.py
@@ -2,4 +2,12 @@ import requests
 
 url = "https://api.coinbase.com/v2/exchange-rates?currency=BTC"
 resp = requests.get(url)
+# print(resp.json()['data']['rates']['USD'])
+
+params = {
+    "currency": "BTC",
+    "base": "USD"
+}
+
+resp = requests.get(url, params=params)
 print(resp.json()['data']['rates']['USD'])
