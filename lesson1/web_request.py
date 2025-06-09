7158381cc83d70010516251c34791596d6c4196e
Liberbell <liberbell@gmail.com>
2025-06-09 11:33:45 +0900

    web_request1

--- lesson1/web_request.py
+++ lesson1/web_request.py
@@ -1,5 +1,9 @@
 from urllib.request import urlopen
 
 url = "https://quotes.toscrape.com/"
-response = urlopen(url)
-print(response.status, response.read().decode("utf-8"))
+# response = urlopen(url)
+# print(response.status, response.read().decode("utf-8"))
+
+with urlopen(url=url) as resp:
+    content = resp.read()
+    print(content.decode("utf-8"))
