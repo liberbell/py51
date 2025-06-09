03de5a26a0b9605236152c2f9944299c9d0691a9
Liberbell <liberbell@gmail.com>
2025-06-10 08:35:18 +0900

    get_with_key3

--- lesson1/get_with_key.py
+++ lesson1/get_with_key.py
@@ -5,3 +5,9 @@ config = configparser.ConfigParser()
 config.read("api_key.ini")
 key1 = config["Key-Values"]["Key1"]
 print(key1)
+
+url = "https://api.exchange.coinbase.com/files"
+headers = {
+    "Authorization": f"Bearer {key1}",
+    "Content-Type": "application/json"
+}
