b2690846100d5cf779628109ea98d000d1f0f9bf
Liberbell <liberbell@gmail.com>
2025-06-09 11:58:14 +0900

    web_requests8

--- lesson1/web_requests.py
+++ lesson1/web_requests.py
@@ -7,5 +7,6 @@ print(resp.content.decode("utf-8"))
 print(resp.headers)
 
 url2 = "https://httpbin.org/headers"
-resp = requests.get(url2)
+resp = requests.get(url2, headers={"User-Agent": "Mozilla/5.0"})
 print(resp.json())
+print(resp.request, resp.request.headers)
