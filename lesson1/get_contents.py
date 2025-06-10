7fb8d649961ffe631a94325aa3e15521afaf37ce
Liberbell <liberbell@gmail.com>
2025-06-11 08:40:19 +0900

    get_contents6

--- lesson1/get_contents.py
+++ lesson1/get_contents.py
@@ -10,7 +10,12 @@ import json
 # resp = requests.put("https://www.httpbin.org/put")
 # print(resp.json())
 
-resp = requests.post("https://www.httpbin.org/post", data={"key1": "value1"})
+resp = requests.post("https://www.httpbin.org/post",
+                     data={
+                         "key1": "value1",
+                         "key2": "value2"
+                         })
 json_string = json.dumps(resp.json(), indent=2)
 print(json_string)
 print(resp.request.body)
+
