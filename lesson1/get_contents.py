e75c96de9bfad4b831a75a07863ecc03f0713a23
Liberbell <liberbell@gmail.com>
2025-06-11 08:32:58 +0900

    get_contents4

--- lesson1/get_contents.py
+++ lesson1/get_contents.py
@@ -1,10 +1,15 @@
 import requests
+import json
 
-resp = requests.delete("https://www.httpbin.org/delete")
-print(resp.json())
+# resp = requests.delete("https://www.httpbin.org/delete")
+# print(resp.json())
 
-resp = requests.patch("https://www.httpbin.org/patch")
-print(resp.json())
+# resp = requests.patch("https://www.httpbin.org/patch")
+# print(resp.json())
 
-resp = requests.put("https://www.httpbin.org/put")
-print(resp.json())
+# resp = requests.put("https://www.httpbin.org/put")
+# print(resp.json())
+
+resp = requests.post("https://www.httpbin.org/post", data={"key1": "value1"})
+json_string = json.dumps(resp.json(), indent=2)
+print(json_string)
