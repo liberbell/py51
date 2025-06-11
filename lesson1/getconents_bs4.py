fadeb3efff8063e109cc789714b7231a0a33658f
Liberbell <liberbell@gmail.com>
2025-06-12 08:19:51 +0900

    getconents_bs44

--- lesson1/getconents_bs4.py
+++ lesson1/getconents_bs4.py
@@ -7,4 +7,8 @@ resp = requests.get(url1)
 # print(resp.content)
 if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
-    print(soup.title)
+    # print(soup.title)
+    # print(soup.h1)
+    print(soup.div)
+    first_div = soup.div
+    print(first_div.div.div)
