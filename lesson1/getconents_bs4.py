6dc91b0ee09583ec033f443d6020878e4d10ad2e
Liberbell <liberbell@gmail.com>
2025-06-13 17:08:34 +0900

    getconents_bs41

--- lesson1/getconents_bs4.py
+++ lesson1/getconents_bs4.py
@@ -15,6 +15,8 @@ if resp.status_code == 200:
     # print(first_div.div.div.attrs)
     # print(list(soup.ul.children))
     print(list(filter(lambda x: type(x) != NavigableString, soup.ul.children)))
+    li_child = no_navigable_strings(soup.ul.children)
+    print(li_child)
 
 def no_navigable_strings(iterable):
     return list(filter(lambda x: type(x) != NavigableString, iterable))
