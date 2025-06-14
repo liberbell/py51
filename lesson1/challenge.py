2ce319758c0a2cd99871b9bd7ea5fb449fa99754
Liberbell <liberbell@gmail.com>
2025-06-14 16:05:58 +0900

    challenge3

--- lesson1/challenge.py
+++ lesson1/challenge.py
@@ -6,3 +6,4 @@ url = "https://books.toscrape.com"
 resp = requests.get(url)
 if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
+    books_tags = soup.find_all("article", attrs={"class": "product_pod"})
