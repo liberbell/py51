7372711464a7c0cc4b749539258e298cdd27d839
Liberbell <liberbell@gmail.com>
2025-06-16 14:37:52 +0900

    challenge6

--- lesson1/challenge.py
+++ lesson1/challenge.py
@@ -9,9 +9,12 @@ def extract_book_data(book_tag):
     rating = book_tag.find("p", attrs={"class": "star-rating"})["class"][-1]
     return title, price, rating
 
+def clean_price(price):
+    return "".join([char for char in price if char.isdigit() or char == '.'])
+
 resp = requests.get(url)
 if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
     books_tags = soup.find_all("article", attrs={"class": "product_pod"})
-    book_title, book_price, book_rating = extract_book_data(books_tags[0])
+    book_title, book_price, book_rating = extract_book_data(books_tags[3])
     print(book_title, book_price, book_rating)
