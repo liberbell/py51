ef89212f923b5505a84fe1ecd5f5cd65d9de9385
Liberbell <liberbell@gmail.com>
2025-06-17 08:05:25 +0900

    challenge1

--- lesson1/challenge.py
+++ lesson1/challenge.py
@@ -4,6 +4,10 @@ import re
 
 url = "https://books.toscrape.com"
 
+
+def clean_price2(price):
+    return float(re.sub("[^0-9.]", "", price))
+
 def extract_book_data(book_tag):
     title = book_tag.find("h3").find("a")["title"]
     price = book_tag.find("p", attrs={"class": "price_color"}).get_text()
@@ -20,14 +24,12 @@ def clean_price(price):
     # return "".join([char for char in price if char.isdigit() or char == '.'])
     return float("".join([char for char in price if char.isdigit() or char == '.']))
 
-def clean_price2(price):
-    return float(re.sub("[^0-9.]", "", price))
 
 resp = requests.get(url)
 if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
     books_tags = soup.find_all("article", attrs={"class": "product_pod"})
     book_title, book_price, book_rating = extract_book_data(books_tags[3])
-    book_price = clean_price2(book_price)
+    # book_price = clean_price2(book_price)
     print(book_title, book_price, book_rating)
     print(type(book_price))
