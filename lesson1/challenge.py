ad569af57e8d949e5757de2e5a67f8fb00b535ef
Liberbell <liberbell@gmail.com>
2025-06-16 14:25:26 +0900

    challenge4

--- lesson1/challenge.py
+++ lesson1/challenge.py
@@ -6,11 +6,11 @@ url = "https://books.toscrape.com"
 def extract_book_data(book_tag):
     title = book_tag.find("h3").find("a")["title"]
     price = book_tag.find("p", attrs={"class": "price_color"}).text
-    return title
+    return title, price
 
 resp = requests.get(url)
 if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
     books_tags = soup.find_all("article", attrs={"class": "product_pod"})
-    book_data = extract_book_data(books_tags[0])
-    print(book_data)
+    book_title, book_price = extract_book_data(books_tags[0])
+    print(book_title, book_price)
