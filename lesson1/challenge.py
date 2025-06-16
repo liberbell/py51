237f11b2b8019e6e7da6274f22b881a0a0c75bf8
Liberbell <liberbell@gmail.com>
2025-06-16 14:47:55 +0900

    challenge12

--- lesson1/challenge.py
+++ lesson1/challenge.py
@@ -8,7 +8,13 @@ def extract_book_data(book_tag):
     title = book_tag.find("h3").find("a")["title"]
     price = book_tag.find("p", attrs={"class": "price_color"}).get_text()
     rating = book_tag.find("p", attrs={"class": "star-rating"})["class"][-1]
-    return title, price, rating
+
+    return {
+        "title": title,
+        "price": clean_price2(price),
+        "rating": rating
+    }
+    # return title, price, rating
 
 def clean_price(price):
     # return "".join([char for char in price if char.isdigit() or char == '.'])
