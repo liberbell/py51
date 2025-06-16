f4c06d90d54bb9abdaf6881d84d4b64a8b6aa228
Liberbell <liberbell@gmail.com>
2025-06-17 08:10:43 +0900

    challenge4

--- lesson1/challenge.py
+++ lesson1/challenge.py
@@ -9,6 +9,16 @@ url = "https://books.toscrape.com"
 def clean_price2(price):
     return float(re.sub("[^0-9.]", "", price))
 
+def convert_rating(rating):
+    ratings = {
+        "One": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5
+    }
+    return ratings.get(rating, 0)
+
 def extract_book_data(book_tag):
     title = book_tag.find("h3").find("a")["title"]
     price = book_tag.find("p", attrs={"class": "price_color"}).get_text()
@@ -25,14 +35,13 @@ def clean_price(price):
     # return "".join([char for char in price if char.isdigit() or char == '.'])
     return float("".join([char for char in price if char.isdigit() or char == '.']))
 
-
 resp = requests.get(url)
 if resp.status_code == 200:
     soup = BeautifulSoup(resp.content, "html.parser")
     books_tags = soup.find_all("article", attrs={"class": "product_pod"})
-    book_title, book_price, book_rating = extract_book_data(books_tags[3])
+    # book_title, book_price, book_rating = extract_book_data(books_tags[3])
     # book_price = clean_price2(book_price)
-    print(book_title, book_price, book_rating)
-    print(type(book_price))
+    # print(book_title, book_price, book_rating)
+    # print(type(book_price))
 
 print(extract_book_data(choice(books_tags)))
