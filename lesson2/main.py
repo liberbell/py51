f3761cdc2e7022ce918a6aa2a1a8bfd50aefb26a
Liberbell <liberbell@gmail.com>
2025-06-21 12:43:33 +0900

    main1

--- lesson2/main.py
+++ lesson2/main.py
@@ -3,5 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_price_information(ticker, exchange):
-    url = "https://www.google.com/finance/quote/HPE:NYSE?authuser=0"
-    # url = "https://www.google.com/finance/quote/{ticker}:{exchange}?authuser=0"
+    # url = "https://www.google.com/finance/quote/HPE:NYSE?authuser=0"
+    url = f"https://www.google.com/finance/quote/{ticker}:{exchange}?authuser=0"
+
+    resp = requests.get(url)
