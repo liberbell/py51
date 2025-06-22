c9f1f8b4859bc91d54b328b80608b1d80f6bffe9
Liberbell <liberbell@gmail.com>
2025-06-22 14:51:43 +0900

    main2

--- lesson2/main.py
+++ lesson2/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
+def get_fx_to_usd(currency):
+    url = f"https://www.google.com/finance/quote/{currency}-USD"
+    
 def get_price_information(ticker, exchange):
     # url = "https://www.google.com/finance/quote/HPE:NYSE?authuser=0"
     url = f"https://www.google.com/finance/quote/{ticker}:{exchange}?authuser=0"
