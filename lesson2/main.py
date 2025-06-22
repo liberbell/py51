810796f50097a599419d18362dca04581f432895
Liberbell <liberbell@gmail.com>
2025-06-22 14:54:00 +0900

    main4

--- lesson2/main.py
+++ lesson2/main.py
@@ -8,6 +8,11 @@ def get_fx_to_usd(currency):
     if resp.status_code == 200:
         soup = BeautifulSoup(resp.content, "html.parser")
         fx_div = soup.find("div", attrs={"data-last-price": True})
+        fx_rate = float(fx_div["data-last-price"])
+        return fx_rate
+    else:
+        print("Request failed.")
+        
 def get_price_information(ticker, exchange):
     # url = "https://www.google.com/finance/quote/HPE:NYSE?authuser=0"
     url = f"https://www.google.com/finance/quote/{ticker}:{exchange}?authuser=0"
