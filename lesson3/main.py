0eb7a8b00244568fd00a68bda82678b519f407aa
Liberbell <liberbell@gmail.com>
2025-06-26 08:14:55 +0900

    main6

--- lesson3/main.py
+++ lesson3/main.py
@@ -2,176 +2,178 @@ import requests
 
 url = "https://use1-prod-th.rbictg.com/graphql"
 
-payload = {
-    "operationName": "GetRestaurants",
-    "variables": {"input": {
-        "filter": "NEARBY",
-        "cordinates": {
-            "userLat": 43.447788,
-            "userLng": -79.3733,
-            "searchRadius": 8000
-        },
-        "first": 20,
-        "status": "OPEN",
-    }},
-    "query": """query GetRestaurants($input: RestaurantsInput) {
-  restaurants(input: $input) {
-    pageInfo {
-      hasNextPage
-      endCursor
-      __typename
-    }
-    totalCount
-    nodes {
-      ...RestaurantNodeFragment
-      __typename
-    }
-    __typename
-  }
-}
-
-fragment RestaurantNodeFragment on RestaurantNode {
-  _id
-  storeId
-  isAvailable
-  posVendor
-  chaseMerchantId
-  cateringHours {
-    ...OperatingHoursFragment
-    ...OperatingHoursFragment
-    __typename
-  }
-  curbsideHours {
-    ...OperatingHoursFragment
-    __typename
-  }
-  cateringHours {
-    ...OperatingHoursFragment
-    __typename
-  }
-  timezone
-  deliveryHours {
-    ...OperatingHoursFragment
-    __typename
-  }
-  diningRoomHours {
-    ...OperatingHoursFragment
-    __typename
-  }
-  distanceInMiles
-  drinkStationType
-  driveThruHours {
-    ...OperatingHoursFragment
-    __typename
-  }
-  driveThruLaneType
-  email
-  environment
-  franchiseGroupId
-  franchiseGroupName
-  frontCounterClosed
-  hasBreakfast
-  hasBurgersForBreakfast
-  hasCatering
-  hasCurbside
-  hasDelivery
-  hasDineIn
-  hasDriveThru
-  hasTableService
-  hasMobileOrdering
-  hasLateNightMenu
-  hasParking
-  hasPlayground
-  hasTakeOut
-  hasWifi
-  hasLoyalty
-  id
-  isDarkKitchen
-  isFavorite
-  isHalal
-  isRecent
-  latitude
-  longitude
-  mobileOrderingStatus
-  name
-  number
-  parkingType
-  phoneNumber
-  physicalAddress {
-    address1
-    address2
-    city
-    country
-    postalCode
-    stateProvince
-    stateProvinceShort
-    __typename
-  }
-  playgroundType
-  pos {
-    vendor
-    __typename
-  }
-  posRestaurantId
-  restaurantImage {
-    asset {
-      _id
-      metadata {
-        lqip
-        palette {
-          dominant {
-            background
-            foreground
-            __typename
-          }
-          __typename
+payload = [
+    {
+        "operationName": "GetRestaurants",
+        "variables": {"input": {
+            "filter": "NEARBY",
+            "cordinates": {
+                "userLat": 43.447788,
+                "userLng": -79.3733,
+                "searchRadius": 8000
+            },
+            "first": 20,
+            "status": "OPEN",
+        }},
+        "query": """query GetRestaurants($input: RestaurantsInput) {
+    restaurants(input: $input) {
+        pageInfo {
+        hasNextPage
+        endCursor
+        __typename
+        }
+        totalCount
+        nodes {
+        ...RestaurantNodeFragment
+        __typename
         }
         __typename
-      }
-      __typename
     }
-    crop {
-      top
-      bottom
-      left
-      right
-      __typename
     }
-    hotspot {
-      height
-      width
-      x
-      y
-      __typename
-    }
-    __typename
-  }
-  restaurantPosData {
-    _id
-    __typename
-  }
-  status
-  vatNumber
-  timezone
-  __typename
-}
 
-fragment OperatingHoursFragment on OperatingHours {
-  friClose
-  friOpen
-  monClose
-  monOpen
-  satClose
-  satOpen
-  sunClose
-  sunOpen
-  thrClose
-  thrOpen
-  tueClose
-  tueOpen
-  wedClose
-  wedOpen
-  __typename
-}
-"""
-}
+    fragment RestaurantNodeFragment on RestaurantNode {
+    _id
+    storeId
+    isAvailable
+    posVendor
+    chaseMerchantId
+    cateringHours {
+        ...OperatingHoursFragment
+        ...OperatingHoursFragment
+        __typename
+    }
+    curbsideHours {
+        ...OperatingHoursFragment
+        __typename
+    }
+    cateringHours {
+        ...OperatingHoursFragment
+        __typename
+    }
+    timezone
+    deliveryHours {
+        ...OperatingHoursFragment
+        __typename
+    }
+    diningRoomHours {
+        ...OperatingHoursFragment
+        __typename
+    }
+    distanceInMiles
+    drinkStationType
+    driveThruHours {
+        ...OperatingHoursFragment
+        __typename
+    }
+    driveThruLaneType
+    email
+    environment
+    franchiseGroupId
+    franchiseGroupName
+    frontCounterClosed
+    hasBreakfast
+    hasBurgersForBreakfast
+    hasCatering
+    hasCurbside
+    hasDelivery
+    hasDineIn
+    hasDriveThru
+    hasTableService
+    hasMobileOrdering
+    hasLateNightMenu
+    hasParking
+    hasPlayground
+    hasTakeOut
+    hasWifi
+    hasLoyalty
+    id
+    isDarkKitchen
+    isFavorite
+    isHalal
+    isRecent
+    latitude
+    longitude
+    mobileOrderingStatus
+    name
+    number
+    parkingType
+    phoneNumber
+    physicalAddress {
+        address1
+        address2
+        city
+        country
+        postalCode
+        stateProvince
+        stateProvinceShort
+        __typename
+    }
+    playgroundType
+    pos {
+        vendor
+        __typename
+    }
+    posRestaurantId
+    restaurantImage {
+        asset {
+        _id
+        metadata {
+            lqip
+            palette {
+            dominant {
+                background
+                foreground
+                __typename
+            }
+            __typename
+            }
+            __typename
+        }
+        __typename
+        }
+        crop {
+        top
+        bottom
+        left
+        right
+        __typename
+        }
+        hotspot {
+        height
+        width
+        x
+        y
+        __typename
+        }
+        __typename
+    }
+    restaurantPosData {
+        _id
+        __typename
+    }
+    status
+    vatNumber
+    timezone
+    __typename
+    }
+
+    fragment OperatingHoursFragment on OperatingHours {
+    friClose
+    friOpen
+    monClose
+    monOpen
+    satClose
+    satOpen
+    sunClose
+    sunOpen
+    thrClose
+    thrOpen
+    tueClose
+    tueOpen
+    wedClose
+    wedOpen
+    __typename
+    }
+    """
+    }
+]
