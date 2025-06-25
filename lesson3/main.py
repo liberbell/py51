8ba1405c2c2d1ac780791b24f48eb45586de75a9
Liberbell <liberbell@gmail.com>
2025-06-26 08:06:43 +0900

    main3

--- lesson3/main.py
+++ lesson3/main.py
@@ -4,4 +4,13 @@ url = "https://use1-prod-th.rbictg.com/graphql"
 
 payload = {
     "operationName": "GetRestaurants",
+    "variables": {"input": {
+        "filter": "NEARBY",
+        "cordinates": {
+            "userLat": 43.447788,
+            "userLng": -79.3733,
+            "searchRadius": 8000
+        }
+    }
+    }
 }
