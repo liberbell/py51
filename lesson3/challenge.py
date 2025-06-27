741228ef97c0c7f86a1398756f6cdec547a387d9
Liberbell <liberbell@gmail.com>
2025-06-27 09:03:06 +0900

    challenge1

---
+++ lesson3/challenge.py
@@ -0,0 +1,3 @@
+import requests
+
+url = "https://api.higherme.com/classic/jobs?page=1&includes=location,location.company,location.externalServiceReferences&limit=24&filters[brand.id]=58bd9e7f472bd&filters[lat]=43.6436&filters[lng]=-79.3757&filters[distance]=20&sort[distance]=asc"
