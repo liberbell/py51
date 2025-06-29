a48040aeda5e505d0ceeb8fbabc0e3e3c2ae1838
Liberbell <liberbell@gmail.com>
2025-06-29 16:17:36 +0900

    challenge1

--- lesson3/challenge.py
+++ lesson3/challenge.py
@@ -27,4 +27,4 @@ def get_job_for(lat=None, lng=None, results=20):
 response_data = get_job_for(43.6532, -79.3832, results=20)
 # print(response_data.get("data"))
 for r in response_data.get("data"):
-    print(r.get("attribute"))
+    print(r.get("attributes"))
