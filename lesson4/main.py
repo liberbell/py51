93bdc840538d9fab845771373fd7d2f670a89c11
Liberbell <liberbell@gmail.com>
2025-06-30 12:36:40 +0900

    main5

--- lesson4/main.py
+++ lesson4/main.py
@@ -1,10 +1,13 @@
 import requests
 from selectolax.parser import HTMLParser
+from random import choice
 
 url = "https://en.wikipedia.org/wiki/Rare-earth_element"
 
 resp = requests.get(url)
 
 tree = HTMLParser(resp.text)
-print(type(tree))
-print(tree.css("p"))
+# print(type(tree))
+# print(tree.css("p"))
+
+random_node = choice(tree.css("img"))
