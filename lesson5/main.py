e86f201cf07fc9c45b2bceb3d6ee2a67660e9360
Liberbell <liberbell@gmail.com>
2025-07-02 08:39:11 +0900

    main5

--- lesson5/main.py
+++ lesson5/main.py
@@ -8,6 +8,10 @@ def get_img_tags_for(term="galaxy"):
     
     if resp.status_code != 200:
         raise Exception("Error getting response")
+    
+    tree = HTMLParser(resp.text)
+    imgs = tree.css("figure a img")
+    return imgs
 
 if __name__ == "__main__":
     print(get_img_tags_for("python"))
