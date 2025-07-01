4362ee3876108239cc00f5c01b3878683d8b8ec5
Liberbell <liberbell@gmail.com>
2025-07-02 08:34:26 +0900

    main4

--- lesson5/main.py
+++ lesson5/main.py
@@ -4,7 +4,10 @@ from selectolax.parser import HTMLParser
 
 def get_img_tags_for(term="galaxy"):
     url = f"https://unsplash.com/s/phtoss/{term}"
-    pass
+    resp = get(url)
+    
+    if resp.status_code != 200:
+        raise Exception("Error getting response")
 
 if __name__ == "__main__":
     print(get_img_tags_for("python"))
