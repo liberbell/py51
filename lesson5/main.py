018ccfada32db75cd5e0dce64356576bcb272faf
Liberbell <liberbell@gmail.com>
2025-07-02 08:31:25 +0900

    main3

--- lesson5/main.py
+++ lesson5/main.py
@@ -3,6 +3,7 @@ from httpx import get
 from selectolax.parser import HTMLParser
 
 def get_img_tags_for(term="galaxy"):
+    url = f"https://unsplash.com/s/phtoss/{term}"
     pass
 
 if __name__ == "__main__":
