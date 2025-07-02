916b8c0f72b0226cba85ef12967bcbd536fb60e9
Liberbell <liberbell@gmail.com>
2025-07-02 18:24:26 +0900

    main7

--- lesson5/main.py
+++ lesson5/main.py
@@ -5,13 +5,17 @@ from selectolax.parser import HTMLParser
 def get_img_tags_for(term):
     url = f"https://unsplash.com/s/phtoss/{term}"
     resp = get(url)
+    print(resp.text)
     
     if resp.status_code != 200:
         raise Exception("Error getting response")
     
     tree = HTMLParser(resp.text)
-    imgs = tree.css("figure a img")
+    imgs = tree.css("figure a img + div img")
     return imgs
 
-if __name__ == "__main__":
-    print(get_img_tags_for("galaxy"))
+
+data = get_img_tags_for("galaxy")
+print(data)
+# if __name__ == "__main__":
+#     print(get_img_tags_for("galaxy"))
