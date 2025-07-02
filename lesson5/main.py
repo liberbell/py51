032fe8a2d6471ff2f448b560447838300fba6463
Liberbell <liberbell@gmail.com>
2025-07-03 08:22:23 +0900

    main3

--- lesson5/main.py
+++ lesson5/main.py
@@ -17,8 +17,8 @@ def get_img_tags_for(term=None):
         raise Exception("Error getting response")
     
     tree = HTMLParser(resp.text)
-    imgs = tree.css("figure a img + div img")
-    # imgs = tree.css("div + div a img")
+    # imgs = tree.css("figure a img + div img")
+    imgs = tree.css("div div a img")
     # //*[@id="«R5iqp6m»"]/div/div/div[2]/figure[1]/div[1]/div/a/img
     return imgs
 
